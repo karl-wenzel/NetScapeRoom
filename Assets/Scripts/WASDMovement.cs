fddc47e2b31ce9d3d89c7acb537eafbd9fe9a827
Entagon <karl-wenzel@gmx.de>
2021-06-10 09:44:05 +0200

    setUp

---
+++ Assets/Scripts/WASDMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WASDMovement: MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W)) {
+            transform.Translate(Vector3.up*Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left*Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right*Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down*Time.deltaTime);
+        }
+    }
+}
